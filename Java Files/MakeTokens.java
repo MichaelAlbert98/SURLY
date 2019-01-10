7c139c2e7d522bf1b477ad64cf20fd047739acc5
Michael Albert <albertm4@wwu.edu>
2019-01-10 09:26:36 -0800

    Upload New File

---
+++ Java Files/MakeTokens.java
@@ -0,0 +1,92 @@
+//MakeTokens.java - Breaks a text file into a list of tokens
+//Created by Michael Albert
+//Created January 09, 2019
+//Revised January 10, 2019
+
+import java.util.*;
+
+public class MakeTokens {
+
+  //constructor
+  private MakeTokens() {
+  }
+
+  //takes a string of text and converts it to tokens
+  public static ArrayList<String> tokenizer(String text) {
+    ArrayList<String> tokenList = new ArrayList<String>();
+    int i = 0;
+    while (i < text.length()) {
+      switch (text.charAt(i)) {
+        case '(':
+          tokenList.add("(");
+          i++;
+          break;
+        case ')':
+          tokenList.add(")");
+          i++;
+          break;
+        case '=':
+          tokenList.add("=");
+          i++;
+          break;
+        case '!':
+          if (text.charAt(i+1) == ('=')) {
+            tokenList.add("!=");
+            i = i + 2;
+          }
+          else {
+            tokenList.add("!");
+            i++;
+          }
+          break;
+        case '<':
+          if (text.charAt(i+1) == ('=')) {
+            tokenList.add("<=");
+            i = i + 2;
+          }
+          else {
+            tokenList.add("<");
+            i++;
+          }
+          break;
+        case '>':
+          if (text.charAt(i+1) == ('=')) {
+            tokenList.add(">=");
+            i = i + 2;
+          }
+          else {
+            tokenList.add(">");
+            i++;
+          }
+          break;
+        case ';':
+          tokenList.add(";");
+          i++;
+          break;
+        case '*':
+          tokenList.add("*");
+          i++;
+          break;
+        case '\'':
+          tokenList.add("'");
+          i++;
+          break;
+        case ',':
+          tokenList.add(",");
+          i++;
+          break;
+        default:
+          String result = "";
+          while ((i < text.length()) && (text.charAt(i) != '(') && (text.charAt(i) != ')') && (text.charAt(i) != '=') && (text.charAt(i) != '(')
+                && (text.charAt(i) != '<') && (text.charAt(i) != '>') && (text.charAt(i) != ';') && (text.charAt(i) != '*')
+                && (text.charAt(i) != '\'') && (text.charAt(i) != ',')) {
+            result = result + text.charAt(i);
+            i++;
+          }
+          tokenList.add(result);
+          break;
+      }
+    }
+    return tokenList;
+  }
+}
