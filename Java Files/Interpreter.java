621a105d75184a0dc5d0215682a5d6845d562763
Jacob Charley Coffland <cofflaj@LabImage2016-17>
2019-01-15 19:13:19 -0800

    added functionallity for keywords

--- Java Files/Interpreter.java
+++ Java Files/Interpreter.java
@@ -32,17 +32,67 @@ public class Interpreter {
   }
 
   private static void relation(ArrayList<String> splitText) {
-    System.out.println("Relation started this token.");
+    int count = 0;
+    i++;
+    String relationName = splitText.get(i);
+    i++;
+    if(!splitText.get(i).equals("(")) {
+      return;
+    }
+    i++;
+    while(!splitText.get(i).equals(")")){
+      i += 3;
+      if(splitText.get(i).equals(",")) {
+         i++;
+      }
+      count++;
+    }
+    System.out.println("Creating " + relationName + " with " + count + " attributes.");
     return;
   }
 
   private static void insert(ArrayList<String> splitText) {
-    System.out.println("Insert started this token.");
+    int count = 0;
+    i++;
+    String insertionName = splitText.get(i);
+    i++;
+    while(!splitText.get(i).equals(";")) {
+      //System.out.println("the " + count + "th item is " + splitText.get(i));
+      i++;
+      count++;
+    }
+    System.out.println("Inserting " + count + " attributes to " + insertionName + ".");
     return;
   }
 
   private static void print(ArrayList<String> splitText) {
-    System.out.println("Print started this token.");
+    int count = 0;
+    i++;
+    while(!splitText.get(i).equals(";")) {
+      i++;
+      while(splitText.get(i).equals(",")){
+         i++;
+      }
+      count++;
+    }
+    System.out.println("Printing " + count + " relations.");
     return;
   }
+  
+  /*private static boolean isKeyword(String s) {
+    if(s.toLowerCase().equals("relation") || s.toLowerCase().equals("insert") || s.toLowerCase().equals("print")) {
+      return true;
+    }
+    return false;
+  }
+
+  private static String analyzeToken(String s) {
+    if(s.toLowerCase().equals("relation") || s.toLowerCase().equals("insert") || s.toLowerCase().equals("print")) {return "keyword";}
+    if(s.toLowerCase().equals("(")){return "open paren";}
+    if(s.toLowerCase().equals(")")){return "close paren";}
+    if(s.toLowerCase().equals(",")){return "comma";}
+    if(s.toLowerCase().equals(";")){return "sc";}
+    return "none";
+  }*/
+  
 }
