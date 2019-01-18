8288e9087692801468578d78e85ca49f384d1591
MichaelAlbert98 <albertm4@wwu.edu>
2019-01-18 00:29:08 -0800

    Added comments.

--- Java Files/ReadIn.java
+++ Java Files/ReadIn.java
@@ -1,7 +1,7 @@
 //ReadIn.java - Turns a given text file into a string, ignoring comments
 //Created by Michael Albert
 //Created January 08, 2019
-//Revised January 09, 2019
+//Revised January 16, 2019
 
 import java.util.*;
 import java.io.FileReader;
@@ -16,7 +16,7 @@ public class ReadIn {
     String result = new String("");
     try {
       //create scanner that ignores comments and newlines
-      Scanner in = new Scanner(new FileReader(fileName + ".txt")).useDelimiter("\\/\\*[^*]*\\*\\/|\\n|\\r");
+      Scanner in = new Scanner(new FileReader(fileName)).useDelimiter("\\/\\*[^*]*\\*\\/|\\n|\\r");
       while(in.hasNext()) {
         result = result + in.next() + " ";
       }
