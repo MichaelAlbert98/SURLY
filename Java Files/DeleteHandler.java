48ae1b0f3494d4c20c978e3c5a02a61de1714e97
MichaelAlbert98 <albertm4@wwu.edu>
2019-01-27 20:07:28 -0800

    Added delete class.

---
+++ Java Files/DeleteHandler.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+
+public class DeleteHandler {
+
+   private DeleteHandler(){}
+
+   public static int delete(ArrayList<String> splitText, LinkedList database, int i) {
+     j = i + 1;
+     k = i + 2;
+     if (k < splitText.size() && !Interpreter.isKeyword(splitText.get(j)) && !Interpreter.isBreakChar(splitText.get(j))
+        && splitText.get(k).trim().equals(";")) {
+        String relationName = splitText.get(j).trim();
+        for (int a = 1; a < database.size(); a++) {
+          if (database.get(a).getName().equals(relationName)) {
+            database.get(a).getTuples().clear();
+          }
+        }
+        System.out.println("Relation name not found.");
+     }
+     System.out.println("Delete command not correctly formatted.");
+     return i;
+   }
+}
