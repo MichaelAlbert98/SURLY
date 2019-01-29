761f53c6896bd5c7c9f8d31e5718c74f84b95e6f
MichaelAlbert98 <albertm4@wwu.edu>
2019-01-28 22:02:32 -0800

    Added Tuple class.

---
+++ Java Files/Tuple.java
@@ -0,0 +1,17 @@
+//Tuple.java - Class for a tuple in the database. Contains a LinkedList of
+//attributes.
+//Created by Michael Albert
+//Created January 27, 2019
+//Revised January 28, 2019
+
+import java.util.*;
+import java.lang.*;
+
+public class Tuple {
+
+  private LinkedList<Attribute> attributes;
+
+   public Tuple(){
+     this.attributes = new LinkedList<Attribute>;
+   }
+}
