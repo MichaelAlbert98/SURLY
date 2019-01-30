721de303023a5aa9c650813deb26e9ebf7e179a8
Jacob Charley Coffland <cofflaj@LabImage2016-17>
2019-01-30 13:04:34 -0800

    nearly functional; prints tuple instead of adding to db

--- Java Files/InsertHandler.java
+++ Java Files/InsertHandler.java
@@ -5,17 +5,30 @@ public class InsertHandler {
 
   private InsertHandler(){}
 
-  public static int insert(ArrayList<String> splitText, int i) {
+  public static int insert(ArrayList<String> splitText, LinkedList<Relation> database, int i) {
     int count = 0; // Number of attrubutes in insertion
     i++;
-    String insertionName = splitText.get(i);
+    String relationName = splitText.get(i);
     if (i < splitText.size() && !Interpreter.isKeyword(splitText.get(i)) && !Interpreter.isBreakChar(splitText.get(i))) {
-      insertionName = splitText.get(i); //make sure insert name is valid
+      relationName = splitText.get(i); //make sure insert name is valid
     }
     else {
       i--;
       return i;
     }
+    Iterator<Relation> dbIterator = database.iterator();
+    Relation r = null;
+    Relation tmp;
+    while(dbIterator.hasNext()) {
+      if((tmp = dbIterator.next()).getName().equals(relationName)) {
+         r = tmp;
+         //System.out.print(r.getName());
+      }
+    }
+    if(r == null) {
+      return i;
+    }
+    Tuple tuple = new Tuple();
     i++;
     while(i < splitText.size() && !splitText.get(i).equals(";")) {
       if(i >= splitText.size()) {
@@ -29,6 +42,9 @@ public class InsertHandler {
       if (Interpreter.isBreakChar(splitText.get(i))) {
         return i; //make sure there are no break chars as tuples
       }
+      Attribute a = new Attribute(splitText.get(i), r.getAttributeType(count), r.getAttributeLength(count));
+      tuple.addAttribute(a);
+      //System.out.print(splitText.get(i) + " " + r.getAttributeType(count) + " " + r.getAttributeLength(count));
       i++;
       count++;
     }
@@ -36,7 +52,8 @@ public class InsertHandler {
       System.out.println("No insert names given.");
       return i;
     }
-    System.out.println("Inserting " + count + " attributes to " + insertionName + ".");
+    System.out.println(tuple);
+    System.out.println("Inserting " + count + " attributes to " + relationName + ".");
     return i;
   }
 
