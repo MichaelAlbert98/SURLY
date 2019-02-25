a0457d0d2b99882b5b115b672375ae37d217c65d
MichaelAlbert98 <albertm4@wwu.edu>
2019-02-24 16:38:00 -0800

    Split up main method.

--- Java Files/InsertHandler.java
+++ Java Files/InsertHandler.java
@@ -1,3 +1,9 @@
+//InsertHandler.java - Reads through token list when INSERT is found,
+//adds tuple to database if formatting is correct.
+//Created by Michael Albert and Jacob Coffland
+//Created January 20, 2019
+//Revised Febuary 24, 2019
+
 import java.util.*;
 import java.lang.*;
 
@@ -9,60 +15,68 @@ public class InsertHandler {
   private final static String ERR_BAD_FORMAT = "Insert format does not match relation format";
   private final static String ERR_DUP_TUPLE = "Duplicate tuple found, not inserted";
   private final static String ERR_INST_CAT = "Cannot insert tuples to Catalog";
+  private int i;
 
   public InsertHandler(){}
 
-  public int insert(ArrayList<String> splitText, LinkedList<Relation> database, int i) {
-
+  public int insert(ArrayList<String> splitText, LinkedList<Relation> database, int j) {
+    this.i = j;
     int count = 0; // Number of attrubutes in insertion
-
-    String relationName = parseRelationName(splitText, i);
-    if(relationName == null) { return i; }
-
+    String relationName = parseRelationName(splitText, this.i);
+    if(relationName == null) {
+      return this.i;
+    }
     Relation r = getRelation(relationName, database);
-    if(r == null) { return i + 1; }
-    if(r.getName().toLowerCase().equals("catalog")){ System.out.println(ERR_INST_CAT); return i; }
-
+    if(r == null) {
+      return this.i + 1;
+    }
+    if(r.getName().toLowerCase().equals("catalog")) {
+      System.out.println(ERR_INST_CAT);
+      return this.i;
+    }
     Tuple tuple = new Tuple();
-    i += 2;
-
-    while(i < splitText.size() && !splitText.get(i).equals(";")) {
-      if(i >= splitText.size()) {
-        System.out.println(ERR_END_REACHED);
-        return i;
-      }
-      if(Helper.isKeyword(splitText.get(i)) || Helper.isBreakChar(splitText.get(i))) {
-        i--;
-        return i; // If keyword is found mid-insert, throw away current insertion and return to interpret()
-      }
-      if(count >= r.getAttributeFormat().size()) {
-        System.out.println(ERR_ATT_OVERFLOW);
-        return i;
+    this.i += 2;
+    while(this.i < splitText.size() && !splitText.get(this.i).equals(";")) {
+      if(checkFormat(splitText,count,r)) {
+        return this.i;
       }
       // Create an attribute object, storing the value in attribute.name, and filling out the rest from the relation
-      Attribute a = new Attribute(splitText.get(i), r.getAttributeType(count), r.getAttributeLength(count));
+      Attribute a = new Attribute(splitText.get(this.i), r.getAttributeType(count), r.getAttributeLength(count));
       tuple.addAttribute(a);
-      i++;
+      this.i++;
       count++;
     }
-
     if (count == 0) {
       System.out.println(ERR_NO_ATT);
-      return i;
+      return this.i;
     }
-
     if (!verifyAttributeFormat(tuple, r.getAttributeFormat())) {
       System.out.println(ERR_BAD_FORMAT);
-      return i;
+      return this.i;
     }
     if(r.contains(tuple)) {
       System.out.println(ERR_DUP_TUPLE);
-      return i;
+      return this.i;
     }
-
     r.addTuple(tuple); // If no errors are found at this point, its safe to add to the tuple to the relation
     System.out.println("Inserting " + count + " attributes to " + relationName + ".");
-    return i;
+    return this.i;
+  }
+
+  private Boolean checkFormat(ArrayList<String> splitText,int count, Relation r) {
+    if(this.i >= splitText.size()) {
+      System.out.println(ERR_END_REACHED);
+      return true;
+    }
+    if(Helper.isKeyword(splitText.get(this.i)) || Helper.isBreakChar(splitText.get(this.i))) {
+      this.i--;
+      return true; // If keyword is found mid-insert, throw away current insertion and return to interpret()
+    }
+    if(count >= r.getAttributeFormat().size()) {
+      System.out.println(ERR_ATT_OVERFLOW);
+      return true;
+    }
+    return false;
   }
 
   // Gets a relation with a given name from a given database
@@ -81,15 +95,15 @@ public class InsertHandler {
   // Checks to make sure that the attribute format of a tuple matches the format of a relation
   private boolean verifyAttributeFormat(Tuple t, LinkedList<Attribute> relationFormat) {
     LinkedList<Attribute> tupleAttributes = t.getAttr();
-    for(int i = 0; i < relationFormat.size(); i++) {
-      if( i >= tupleAttributes.size() ) {
+    for(int j = 0; j < relationFormat.size(); j++) {
+      if( j >= tupleAttributes.size() ) {
          return false;
       }
-      if( tupleAttributes.get(i).getName().length() > relationFormat.get(i).getLength() ) {
+      if( tupleAttributes.get(j).getName().length() > relationFormat.get(j).getLength() ) {
          return false;
       }
-      if( tupleAttributes.get(i).getDataType().toLowerCase().equals("num") ) {
-         if(!isNum(tupleAttributes.get(i).getName())) {
+      if( tupleAttributes.get(j).getDataType().toLowerCase().equals("num") ) {
+         if(!isNum(tupleAttributes.get(j).getName())) {
             return false;
          }
       }
@@ -98,10 +112,10 @@ public class InsertHandler {
   }
 
   // Checks to make sure a string does not contain a keyword or break char
-  private String parseRelationName(ArrayList<String> splitText, int i) {
-    i++;
-    if (i < splitText.size() && !Helper.isKeyword(splitText.get(i)) && !Helper.isBreakChar(splitText.get(i))) {
-      return splitText.get(i).toLowerCase();
+  private String parseRelationName(ArrayList<String> splitText, int j) {
+    j++;
+    if (j < splitText.size() && !Helper.isKeyword(splitText.get(j)) && !Helper.isBreakChar(splitText.get(j))) {
+      return splitText.get(j).toLowerCase();
     }
     return null;
   }
