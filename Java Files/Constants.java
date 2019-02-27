dd8cb4e7e5be023691fe988a17658449119ebc55
albertm4 <albertm4@wwu.edu>
2019-02-27 12:42:27 -0800

    Added class which contains Surly's constants

---
+++ Java Files/Constants.java
@@ -0,0 +1,26 @@
+//Constants.java - List of constants used by Surly
+//Created by Michael Albert
+//Created Febuary 27, 2019
+//Revised January 27, 2019
+public class Constants {
+
+  //error messages
+  public final static String ERR_END_REACHED = "End of document reached.";
+  public final static String ERR_BAD_FORMAT = "Format is not correct.";
+  public final static String ERR_NOT_FND = "Could not find the specified name.";
+  public final static String ERR_DUP_ITEM = "This name already exists.";
+  public final static String ERR_ATT_OVERFLOW = "Too many attributes for selected relation";
+  public final static String ERR_NO_ATT = "No insert names given.";
+  public final static String ERR_INST_CAT = "Cannot insert tuples to Catalog";
+
+  //string literals
+  public final static String CATALOG = "catalog";
+  public final static String RELATION = "relation";
+  public final static String INSERT = "insert";
+  public final static String PRINT = "print";
+  public final static String DESTROY = "destroy";
+  public final static String DELETE = "delete";
+  public final static String CHAR = "char";
+  public final static String NUM = "num";
+
+}
