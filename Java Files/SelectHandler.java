3f2d909cf02b5909bbc4fa4e6bdbcd5a3ad08ccf
MichaelAlbert98 <albertm4@wwu.edu>
2019-03-03 13:53:43 -0800

    Shortened select method, made sure it works with AND/ORS

--- Java Files/SelectHandler.java
+++ Java Files/SelectHandler.java
@@ -9,48 +9,30 @@ import java.lang.*;
 
 public class SelectHandler {
 
+  private int i;
+  private String relName;
+  private String tempName;
+
    public SelectHandler(){}
 
-   public int select(ArrayList<String> splitText, LinkedList<Relation> database, int i) {
+   public int select(ArrayList<String> splitText, LinkedList<Relation> database, int ix) {
+     this.i = ix;
+     Where where = new Where();
+     tempName = splitText.get(i-2);
 
-     String relationName = "";
-     String tempName = splitText.get(i-2);
+     if (!formatCheck(splitText)) {
+       return this.i;
+     }
+
      Relation tempRel = new Relation(tempName);
      tempRel.setTemp(true);
 
-     //make sure formatting is valid
-     if (!splitText.get(i-1).equals("=") || (Helper.isBreakChar(tempName) || Helper.isKeyword(tempName))) {
-       System.out.println(Constants.ERR_BAD_FORMAT);
-       return i;
-     }
-
-     i++;
-
-     if (i < splitText.size() && !Helper.isKeyword(splitText.get(i)) && !Helper.isBreakChar(splitText.get(i))) {
-       relationName = splitText.get(i);
-     }
-     else {
-       System.out.println(Constants.ERR_BAD_FORMAT);
-       i--;
-       return i;
-     }
-
-     i++;
-
-     if (!splitText.get(i).equals(";") && !splitText.get(i).toLowerCase().equals("where")) {
-       System.out.println(Constants.ERR_BAD_FORMAT);
-       i--;
-       return i;
-     }
-
      //find tuples that match where conditions and make temp relation out of them.
      if (splitText.get(i).toLowerCase().equals("where")) {
-       Where where = new Where();
-
        if (where.whereFormat(splitText,i)) {
          ArrayList<Boolean> selectList = where.whereFind(splitText,database,i);
          for (int j = 1; j < database.size(); j++) {
-           if (database.get(j).getName().equals(relationName)) {
+           if (database.get(j).getName().equals(relName)) {
              tempRel.setAF(database.get(j).getAttributeFormat());
              for (int k=0; k<selectList.size(); k++) {
                if (selectList.get(k)) {
@@ -75,7 +57,7 @@ public class SelectHandler {
      //select all tuples in given relation
      else {
        for (int j = 1; j < database.size(); j++) {
-         if (database.get(j).getName().equals(relationName)) {
+         if (database.get(j).getName().equals(relName)) {
            tempRel.setAF(database.get(j).getAttributeFormat());
            for (int k = 0; k < database.get(j).getTuples().size(); k++) {
              Tuple copy = database.get(j).getTuples().get(k).deepCopy();
@@ -89,4 +71,31 @@ public class SelectHandler {
        return i;
      }
    }
-}
+
+   private boolean formatCheck(ArrayList<String> splitText) {
+     if (!splitText.get(i-1).equals("=") || (Helper.isBreakChar(tempName) || Helper.isKeyword(tempName))) {
+       System.out.println(Constants.ERR_BAD_FORMAT);
+       return false;
+     }
+
+     i++;
+
+     if (i < splitText.size() && !Helper.isKeyword(splitText.get(i)) && !Helper.isBreakChar(splitText.get(i))) {
+       relName = splitText.get(i);
+     }
+     else {
+       System.out.println(Constants.ERR_BAD_FORMAT);
+       i--;
+       return false;
+     }
+
+     i++;
+
+     if (!splitText.get(i).equals(";") && !splitText.get(i).toLowerCase().equals("where")) {
+       System.out.println(Constants.ERR_BAD_FORMAT);
+       i--;
+       return false;
+     }
+     return true;
+   }
+}  
