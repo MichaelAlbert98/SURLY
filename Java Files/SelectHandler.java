8e550e29aad7d3f62b4a5d1ba90ce7e068f5838d
MichaelAlbert98 <albertm4@wwu.edu>
2019-03-03 23:26:29 -0800

    Names equal to temp names overwrite, names equal to non-temp names print error message.

--- Java Files/SelectHandler.java
+++ Java Files/SelectHandler.java
@@ -1,8 +1,8 @@
-//DeleteHandler.java - Iterates through the database for a given name. If a
-//relation with the name is found, all tuples in that relation are removed.
+//DeleteHandler.java - Creates a temp relation from a given relation based on a
+//set of conditions.
 //Created by Michael Albert
-//Created January 27, 2019
-//Revised January 28, 2019
+//Created Febuary 27, 2019
+//Revised March 3, 2019
 
 import java.util.*;
 import java.lang.*;
@@ -12,11 +12,13 @@ public class SelectHandler {
   private int i;
   private String relName;
   private String tempName;
+  private LinkedList<Relation> database;
 
    public SelectHandler(){}
 
    public int select(ArrayList<String> splitText, LinkedList<Relation> database, int ix) {
      this.i = ix;
+     this.database = database;
      Where where = new Where();
      tempName = splitText.get(i-2);
 
@@ -31,7 +33,7 @@ public class SelectHandler {
      if (splitText.get(i).toLowerCase().equals("where")) {
        if (where.whereFormat(splitText,i)) {
          ArrayList<Boolean> selectList = where.whereFind(splitText,database,i);
-         for (int j = 1; j < database.size(); j++) {
+         for (int j = 0; j < database.size(); j++) {
            if (database.get(j).getName().equals(relName)) {
              tempRel.setAF(database.get(j).getAttributeFormat());
              for (int k=0; k<selectList.size(); k++) {
@@ -40,35 +42,49 @@ public class SelectHandler {
                  tempRel.getTuples().add(copy);
                }
              }
+             //can't overwrite non-temp relations
+             if (tempName.equals(relName) && !database.get(j).getTemp()) {
+               System.out.println(Constants.ERR_NAME_OVERWRITE);
+               return Helper.findSemicolon(splitText, this.i);
+             }
+             //delete old temp relation
+             else if (tempName.equals(relName) && database.get(j).getTemp()) {
+               database.remove(j);
+             }
+             database.add(tempRel);
            }
          }
-         database.add(tempRel);
        }
        else { //format not correct, return
          System.out.println(Constants.ERR_BAD_FORMAT);
        }
-
-       while (!splitText.get(i).equals(";")) {
-         i++;
-       }
-       return i;
+       return Helper.findSemicolon(splitText, this.i);
      }
 
      //select all tuples in given relation
      else {
-       for (int j = 1; j < database.size(); j++) {
+       for (int j = 0; j < database.size(); j++) {
          if (database.get(j).getName().equals(relName)) {
            tempRel.setAF(database.get(j).getAttributeFormat());
            for (int k = 0; k < database.get(j).getTuples().size(); k++) {
              Tuple copy = database.get(j).getTuples().get(k).deepCopy();
              tempRel.getTuples().add(copy);
            }
+           //can't overwrite non-temp relations
+           if (tempName.equals(relName) && !database.get(j).getTemp()) {
+             System.out.println(Constants.ERR_NAME_OVERWRITE);
+             return this.i;
+           }
+           //delete old temp relation
+           else if (tempName.equals(relName) && database.get(j).getTemp()) {
+             database.remove(j);
+           }
            database.add(tempRel);
-           return i;
+           return this.i;
          }
        }
        System.out.println(Constants.ERR_NOT_FND);
-       return i;
+       return this.i;
      }
    }
 
@@ -98,4 +114,15 @@ public class SelectHandler {
      }
      return true;
    }
-}  
+
+   private Relation getRelation(String s) {
+      ListIterator<Relation> l = database.listIterator();
+      while(l.hasNext()) {
+         Relation r = l.next();
+        if(r.getName().equals(s)) {
+            return r;
+         }
+      }
+      return null;
+   }
+}
