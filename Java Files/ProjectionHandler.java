f08451013d1ee477f133cd7601c06ccb5af5602a
albertm4 <albertm4@wwu.edu>
2019-03-04 14:00:38 -0800

    Changes

--- Java Files/ProjectionHandler.java
+++ Java Files/ProjectionHandler.java
@@ -5,12 +5,50 @@ public class ProjectionHandler {
    public ProjectionHandler(){}
    private LinkedList<Relation> db;
    
-   public void project (ArrayList<String> tokens, LinkedList<Relation> database, int ix) {
+   public int project (ArrayList<String> tokens, LinkedList<Relation> database, int ix) {
       db = database;
       String name = parseName(tokens, ix);
       Relation relation = parseRelation(tokens, ix);
       LinkedList<Attribute> attributes = parseAttributes(tokens, relation, ix);
+      LinkedList<Boolean> keptAttributes = getKeptAttributes(attributes, relation);
+      // System.out.println("name: " + name + "\nRelation: " + relation + "\nAttributes" + attributes);
       
+      Relation projection = new Relation(name, attributes);
+      
+      ListIterator<Tuple> li = relation.getTuples().listIterator();
+      while(li.hasNext()){
+         ListIterator<Attribute> ti = li.next().getAttr().listIterator();
+         while(ti.hasNext()) {
+            Attribute a = ti.next();
+            //System.out.println("attribute: " + a);
+            if(projectionUsesAttribute(attributes, a)) {
+               // add the attribute to the tuple
+               System.out.println(a);
+            }
+         }
+      }
+      
+      System.out.println("projection is \n" + projection);
+      
+      return ix + 1;
+   }
+   
+   private LinkedList<Boolean> getKeptAttributes(LinkedList<Attribute> atts, Relation r) {
+      ListIterator<Attribute> ri = r.getAttributeFormat().listIterator();
+      LinkedList<Boolean> keptAttributes = new LinkedList<Boolean>();
+      while(ri.hasNext()) {
+         Boolean found = new Boolean(false);
+         Attribute relationAttribute = ri.next();
+         ListIterator<Attribute> ai = atts.listIterator();
+         while(ai.hasNext()) {
+            if(relationAttribute.getName().equals(ai.next().getName())) {
+               found = true;
+            }
+         }
+         keptAttributes.push(found);
+      
+      }
+      return null;
    }
    
    private String parseName(ArrayList<String> tokens, int ix) {
@@ -28,6 +66,7 @@ public class ProjectionHandler {
    }
    
    private LinkedList<Attribute> parseAttributes(ArrayList<String> tokens, Relation r, int ix) {
+      Stack<Attribute> attributeStack = new Stack<Attribute>();
       LinkedList<Attribute> attributes = new LinkedList<Attribute>();
       LinkedList<Attribute> relationAttributes = r.getAttributeFormat();
       do{
@@ -35,12 +74,14 @@ public class ProjectionHandler {
          String att = tokens.get(ix);
          Attribute attribute = getAttribute(relationAttributes, att);
          if(attribute != null) {
-            attributes.push(attribute);
+            attributeStack.push(attribute);
          }
          ix++;
       }
       while (tokens.get(ix).equals(","));
-      String s = tokens.get(ix);
+      while(!attributeStack.empty()) {
+         attributes.push(attributeStack.pop());
+      }
       return attributes;
    }
    
@@ -69,6 +110,17 @@ public class ProjectionHandler {
       }
       while(r == null);
       return null;
+   }
+   
+   private boolean projectionUsesAttribute(LinkedList<Attribute> atts, Attribute a) {   
+      ListIterator<Attribute> li = atts.listIterator();
+      while(li.hasNext()) {
+         if(li.next().getName().equals(a.getName())) {
+            return true;
+         }   
+      }
+      System.out.println("couldnt find name " + a.getName());
+      return false;
    }
    
    public Relation getRelation(String s) {
