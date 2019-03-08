19015636e9eb1ecc9047ceebd28e807c356cd0ab
Jacob Charley Coffland <cofflaj@LabImage2016-17>
2019-03-07 20:02:51 -0800

    ver 0.1 : does parsing, checks syntax. Robust.

---
+++ Java Files/JoinHandler.java
@@ -0,0 +1,155 @@
+import java.util.*;
+
+public class JoinHandler {
+   
+   private LinkedList<Relation> db;
+   
+   public JoinHandler(){}
+   public int join(ArrayList<String> tokens, LinkedList<Relation> database, int i) {
+      
+      db = database;
+      // Check to make sure there are at least 2 tokens before i and 8 after
+      int valid = validateIndex(tokens, i);
+      if(valid != 0) { return valid; }
+      
+      // Parse and validate each of the 11 parts of the command
+      // Part 1 - Name
+      String name = tokens.get(i-2);
+      if(illegalName(name)) { return i + 1; }
+      // Part 2 - '='
+      if(!tokens.get(i - 1).equals("=")) {
+         System.out.println(Constants.ERR_JOIN_SYNTAX);
+         return i + 1;
+      }
+      // Part 3 - JOIN (note: this check shold never fail)
+      if(!tokens.get(i).toLowerCase().equals("join")) {
+         System.out.println(Constants.ERR_JOIN_SYNTAX);
+         return i + 1;
+      }
+      // Part 4 - Relation A
+      Relation relA = getRelation(tokens.get(i+1));
+      if(relA == null) { return i + 1; }
+      // Part 5 - ','
+      if(!tokens.get(i + 2).equals(",")) {
+         System.out.println(Constants.ERR_JOIN_SYNTAX);
+         return i + 2;
+      }
+      // Part 6 - Relation B
+      Relation relB = getRelation(tokens.get(i+3));
+      if(relB == null) { return i + 3; }
+      // Part 7 - ON
+      if(!tokens.get(i + 4).toLowerCase().equals("on")) {
+         System.out.println(Constants.ERR_JOIN_SYNTAX);
+         return i + 4;
+      }
+      // Part 8 - Relation A Attribute
+      Attribute attA = getAttribute(relA, tokens.get(i + 5), tokens.get(i + 7));
+      if(attA == null) { return i + 5; }
+      // Part 9 - '='
+      if(!tokens.get(i + 6).equals("=")) {
+         System.out.println(Constants.ERR_JOIN_SYNTAX);
+         return i + 6;
+      }
+      // Part 10 - Relation Attribute 2
+      Attribute attB = getAttribute(relB, tokens.get(i + 5), tokens.get(i + 7));
+      if(attB == null) { return i + 7; }
+      // Part 11 - ';'
+      if(!tokens.get(i + 8).equals(";")) {
+         System.out.println(Constants.ERR_JOIN_SYNTAX);
+         return i + 8;
+      }
+      
+      System.out.println("Trying to join! Syntax is good, the join name is gonna be " + name + 
+         ", the relations are " + relA.getName() + " and " + relB.getName() +
+         ",\nand the attributes to match on are " + relA.getName() + "." + attA.getName() + 
+         " and " + relB.getName() + "." + attB.getName());
+      
+      return i + 9;
+   }
+   
+   private Attribute getAttribute(Relation r, String a, String b) {
+      
+      int dot = getDotIndex(a);
+      if(dot <= 0) { return null; }
+      // Get the relation given before the dot in a
+      Relation preDot = getRelation(a.substring(0,dot));
+      Attribute ret = getAttribute(r, a.substring(dot + 1,a.length()));
+      // If the relation in string a was not found, try b
+      if(ret == null) {
+         dot = getDotIndex(b);
+         if(dot <= 0) { return null; }
+         preDot = getRelation(b.substring(0,dot));
+         if(preDot == null) {
+            return null;
+         }
+         ret = getAttribute(r, b.substring(dot + 1,b.length()));
+      }
+      if(ret == null) {
+         System.out.println(Constants.ERR_NOT_FND);
+      }
+      return ret;
+   }
+   
+   private Attribute getAttribute(Relation r, String s) {
+      ListIterator<Attribute> li = r.getAttributeFormat().listIterator();
+      while(li.hasNext()) {
+         Attribute a = li.next();
+         if(a.getName().toLowerCase().equals(s.toLowerCase())) {
+            return a;
+         }
+      }
+      return null;
+   }
+   
+   private int getDotIndex(String s) {
+      int ix;
+      for(ix = 0; ix < s.length(); ix++) {
+         if(s.charAt(ix) == '.') {
+            return ix;
+         }
+      }
+      return -1;
+   }
+   
+   private Relation getRelation(String s) {
+      ListIterator<Relation> ri = db.listIterator();
+      while(ri.hasNext()) {
+         Relation r = ri.next();
+         if(s.toLowerCase().equals(r.getName())) {
+            return r;
+         }
+      }
+      System.out.println(Constants.ERR_NOT_FND);
+      return null;
+   }
+   
+   // Check to make sure there are at least 2 tokens before i and 8 after
+   private int validateIndex(ArrayList<String> tokens, int i) {
+      if(i < 2 ) {
+         System.out.println(Constants.ERR_JOIN_SYNTAX);
+         return 2;
+      }
+      if(i + 8 >= tokens.size()) {
+         System.out.println(Constants.ERR_END_REACHED);
+         return i + 1;
+      }
+      return 0;
+   }
+   
+   // Checks if the given name is in use by a non temporary relation or is a keyword/break char
+   private boolean illegalName(String n) {
+      if(Helper.isBreakChar(n) || Helper.isKeyword(n)) {
+         return true; // Error messages handled by Helper
+      }
+      ListIterator<Relation> it = db.listIterator();
+      while(it.hasNext()) {
+         Relation r = it.next();
+         // If the name is in use by a non temporary relation, its illegal
+         if(!r.getTemp() && r.getName().equals(n.toLowerCase())) {
+            System.out.println(Constants.ERR_NAME_OVERWRITE);
+            return true;
+         }
+      }
+      return false;
+   }
+}
